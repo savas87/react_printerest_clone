301e29467f6baf7aa82271a9be4bb84062268b0d
Savas Erbas <savas-erbas@live.de>
2020-02-06 11:40:31 +0100

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'https://api.unsplash.com',    
     headers: {
-        Authorization :'Client-ID 6eab4cafadfaf429c9b6fcfccdf9ce532760d0acad13ea294abbc1e97026821f'
+        Authorization :'Client-ID your-unsplash-api-key'
     }
 
 });
